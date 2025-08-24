3993c5aa2b66b32f6bd15a0f0372822989656039
Omas Ajiri <jicintech@gmail.com>
2025-08-24 15:51:43 +0100

    feat: commit setup of app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1>Welcome to AI resume Analyzer</h1>{" "}
+      </div>
+    </>
+  );
+}
+export default App;
